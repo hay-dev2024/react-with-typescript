f39e1a38cd3591f89770887ce3ed5588c830e16f
Hay <hay.dev2024@gmail.com>
2025-08-03 20:24:44 +0900

    inheretance

---
+++ 02.OOP/PoodleClass/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    class Dog
+    {
+        protected int eyes, nose, mouth, ears;
+        protected string kinds;
+        public Dog()
+        {
+            eyes = 2;
+            nose = 1;
+            mouth = 1;
+            ears = 2;
+            kinds = "개";
+            Console.WriteLine($"{kinds} Initialised: Eyes = {eyes}, Nose = {nose}, Mouth = {mouth}, Ears = {ears}");
+        }
+        public void Bark()
+        {
+            Console.WriteLine("멍멍");
+        }
+    }
+    class Poodle : Dog
+    {
+        public Poodle()
+        {
+            base.kinds = "푸들";
+            Console.WriteLine($"{kinds} Initialised: Eyes = {eyes}, Nose = {nose}, Mouth = {mouth}, Ears = {ears}");
+        }
+
+    }
+    private static void Main(string[] args)
+    {
+        Dog a = new Dog();
+        a.Bark();
+        Poodle pd = new Poodle();
+
+    }
+}
--- str.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    char str[12] = "Hello World";
-    printf("%s\n", str);
-
-    return 0;
-}
