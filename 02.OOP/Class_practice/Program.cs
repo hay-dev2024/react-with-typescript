c3e9544a79b80a8ebe9db1aff424260cd9ae5bb3
Hay <hay.dev2024@gmail.com>
2025-08-03 14:20:16 +0900

    encapsulation

---
+++ 02.OOP/Class_practice/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    class Dog
+    {
+        private int eyes, nose, mouth, ears;
+        public Dog()
+        {
+            eyes = 2;
+            nose = 1;
+            mouth = 1;
+            ears = 2;
+            Console.WriteLine($"Dog Initialised: Eyes = {eyes}, Nose = {nose}, Mouth = {mouth}, Ears = {ears}");
+        }
+        public void Bark()
+        {
+            Console.WriteLine("멍멍!");
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        Dog a = new Dog();
+        a.Bark();
+
+    }
+}
