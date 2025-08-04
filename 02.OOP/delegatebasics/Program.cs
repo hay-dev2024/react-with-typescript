e818a9cfbe1d1a84c658c7b51ec6ba0b1e5835c0
Hay <hay.dev2024@gmail.com>
2025-08-05 00:39:55 +0900

    delegate, arrow func

---
+++ 02.OOP/delegatebasics/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    delegate int CalDel(int a, int b);
+
+    private static void Main(string[] args)
+    {
+        // int hap = Sum(10, 20);
+        // Console.WriteLine(hap);
+
+        // CalDel d = delegate (int a, int b) { return a + b; };
+        CalDel d = (a, b) => a + b;
+        Console.WriteLine(d(10, 20));
+    }
+}
