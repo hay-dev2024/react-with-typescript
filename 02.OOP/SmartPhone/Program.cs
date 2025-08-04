f05042e28a1107f34c39ca5e5e0577cc26862965
Hay <hay.dev2024@gmail.com>
2025-08-05 00:29:51 +0900

    virtual class

--- 02.OOP/SmartPhone/Program.cs
+++ 02.OOP/SmartPhone/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    public interface Call
+    {
+        void callPhone();
+    }
+    public interface Text
+    {
+        void sms();
+    }
+    public interface Wifi
+    {
+        void wifi();
+    }
+    class SmartPhone : Call, Text, Wifi
+    {
+        public void callPhone()
+        {
+            Console.WriteLine("전화걸기");
+        }
+        public void sms()
+        {
+            Console.WriteLine("문자보내기");
+        }
+        public void wifi()
+        {
+            Console.WriteLine("와이파이연결하기");
+        }
+    }
+    private static void Main(string[] args)
+    {
+        SmartPhone sp = new SmartPhone();
+        sp.callPhone();
+        sp.sms();
+        sp.wifi();
+    }
+}
